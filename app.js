4520fbb531bdcefff7df3348b8dc051b1892a618
Ikechukwu Njoku <ikthegenius@yahoo.com>
2017-09-21 23:07:51 +0100

    Absolutely broke everything

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require("express");
-var config = require("./server/configure");
-var app = express();
-var port = process.env.port || 3000;
+var express = require('express'),
+    config = require('./server/configure'),
+    app = express();
 
-
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app = config(app);
 
-
-app.listen(port, function() {
-    console.log("Server up: http://localhost:" + port)
+var server = app.listen(app.get('port'), function() {
+    console.log('Server up: http://localhost:' + app.get('port'));
 });
