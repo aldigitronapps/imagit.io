f44b102d8422efa4d954aa7789038bcce0ab9358
Ikechukwu Njoku <ikthegenius@yahoo.com>
2017-09-28 12:42:37 +0100

    tests wtih mocha

---
+++ tests/mocha.test.js
@@ -0,0 +1,7 @@
+describe("Mocha", function() {
+    describe("First Test", function() {
+        it("should assert 1 equals 1", function() {
+            expect(1).to.equal(1);
+        });
+    });
+});
---
+++ tests/server/routes.test.js
@@ -0,0 +1,42 @@
+var home = require('../../controllers/home'),
+    image = require('../../controllers/image'),
+    routes = require('../../server/routes');
+
+describe('Routes', function() {
+    var app = {
+        get: sinon.spy(),
+        post: sinon.spy(),
+        delete: sinon.spy()
+    };
+    beforeEach(function() {
+        routes.initialize(app);
+    });
+    // to do: write tests...
+    describe('GETs', function() {
+        it('should handle /', function() {
+            expect(app.get).to.be.calledWith('/', home.index);
+        });
+        it('should handle /images/:image_id', function() {
+            expect(app.get).to.be.calledWith('/images/:image_id', image.index);
+        });
+    });
+    //Then, test the POST endpoints:
+    describe('POSTs', function() {
+        it('should handle /images', function() {
+            expect(app.post).to.be.calledWith('/images', image.create);
+        });
+        it('should handle /images/:image_id/like', function() {
+            expect(app.post).to.be.calledWith('/images/:image_id/like', image.like);
+        });
+        it('should handle /images/:image_id/comment', function() {
+            expect(app.post).to.be.calledWith('/images/:image_id/comment', image.comment);
+        });
+    });
+    //Finally, test the DELETE endpoint:
+    describe('DELETEs', function() {
+        it('should handle /images/:image_id', function() {
+            expect(app.delete).to.be.calledWith('/images/:image_id', image.remove);
+        });
+    });
+
+});
